8a6fcb764734f23977a740f4a9aafe764bdf4719
Kottin <Rodrigue.Kottin@transports.gouv.qc.ca>
2021-10-27 16:19:46 -0400

    Debut du projet : Ajout des classes Base64 et d'un petit projet de test

---
+++ Base64/Base64.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base64
+{
+    public static class Base64
+    {
+        public static string Encode(byte[] source)
+        {
+            return "";
+        }
+    }
+}
